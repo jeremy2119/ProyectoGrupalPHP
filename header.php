05fd0c2c02554a1c5adcfca7729e7c713235c3d0
Jeremy Monge Solís <jeremy.monge@labwebcr.com>
2018-11-05 10:03:09 -0600

    cambios

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/style.css" type="text/css">
+    <link rel="stylesheet" href="assets/flexboxgrid.min.css" type="text/css">
     <link rel="stylesheet" href="assets/inicio.css" type="text/css">
     <title>Café Au Lait</title>
     <script src="assets/jquery.js"></script>
